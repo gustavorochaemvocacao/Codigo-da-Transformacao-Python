73ba9ad5150f784136486d1cca21cba0d0c2637b
Gustavo Rocha <gustavorocha.180909@gmail.com>
2026-04-13 13:39:29 -0300

    modulo 2
    lição plataforma

---
+++ modulo_02/ead.py
@@ -0,0 +1,8 @@
+'''
+lição plaaforma
+criar um programa que pede um nome de usuário e exibe uma mensagem personalizada 
+
+'''
+print('digite seu nome de usuário')
+nome_usuario = input()
+print('Olá, ' + nome_usuario + '! Seja bem-vindo ao curso do codigo da trasformação na vocação🤞!')
